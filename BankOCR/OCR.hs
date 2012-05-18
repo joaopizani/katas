b2d7acf4a9f58b98cd873fe46188c89a2717055f
João Paulo Pizani Flor <joaopizani@gmail.com>
2012-05-18 10:54:34 -0300

    BankOCR Haskell kata added

---
+++ BankOCR/OCR.hs
@@ -0,0 +1,44 @@
+module Main where
+
+import Data.List
+import Data.Maybe
+import Data.List.Split
+
+main = interact (\input -> parse input ++ "\n")
+
+parse i = map (getDigit . transpose . tail) $ chunk 4 $ dummy : columns i
+    where getDigit = fromJust . (flip lookup) dTable
+          columns  = transpose . lines
+          dummy    = "   "
+
+dTable = [
+           ([" _ ",
+             "| |",
+             "|_|"], '0'),
+           (["   ",
+             "  |",
+             "  |"], '1'),
+           ([" _ ",
+             " _|",
+             "|_ "], '2'),
+           ([" _ ",
+             " _|",
+             " _|"], '3'),
+           (["   ",
+             "|_|",
+             "  |"], '4'),
+           ([" _ ",
+             "|_ ",
+             " _|"], '5'),
+           ([" _ ",
+             "|_ ",
+             "|_|"], '6'),
+           ([" _ ",
+             "  |",
+             "  |"], '7'),
+           ([" _ ",
+             "|_|",
+             "|_|"], '8'),
+           ([" _ ",
+             "|_|",
+             " _|"], '9') ]
